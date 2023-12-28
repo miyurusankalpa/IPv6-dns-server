e8d69661b781e18cdc767794d93e9f48076d19fb
Miyuru Sankalpa <miyuru@miyuru.lk>
2023-12-28 23:54:49 +0530

    add ip to serve

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const {
 const resolver = new Resolver();
 const resolver_own = new Resolver();
 
-const self_server_and_port = config.self_resolver+":"+config.self_port;
+const self_server_and_port = [config.self_resolver]+":"+config.self_port;
 
 resolver.setServers([dns_resolver]);
 resolver_own.setServers(self_server_and_port);
@@ -47,7 +47,7 @@ server6.on('close', () => console.log('server closed', server6.address()));
 server6.on('error', (err, buff, req, res) => console.error(err.stack));
 server6.on('socketError', (err, socket) => console.error(err));
 
-server6.serve(config.self_port);
+server6.serve(config.self_port, config.self_resolver);
 
 let authority = {
     address: dns_resolver,
