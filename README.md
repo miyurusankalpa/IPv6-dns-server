bee76bbed965ef7108d51ab256bb1a9fecf6d74e
Miyuru Sankalpa <miyuru@miyuru.lk>
2023-12-29 23:04:51 +0530

    add region name detection
    at this point even I don't know what happens in the code. maybe I should use more readable variables and comments..
    
    long live tests!!!

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ Change the `v6_only` variable to true in the app.js file.
 
 * Test domains: oss.aliyuncs.com, alicloud-common.oss-ap-southeast-1.aliyuncs.com, docs-aliyun.cn-hangzhou.oss.aliyun-inc.com
 * IPv6 Type: Unicast
-* Coverage: All from aliyuncs.com and aliyun-inc.com except examplebucket.oss.aliyuncs.com format. (check tests for more info)
+* Coverage: All from aliyuncs.com and aliyun-inc.com
 * Usability: Stable
 
 ## AliCDN 
--- providers/alibabaoss.js
+++ providers/alibabaoss.js
@@ -18,51 +18,95 @@ module.exports = {
             var dpff1 = sdomains.indexOf("oss");
             //console.log("dpff1: "+dpff1);
     
-            var ssdomains = sdomains[2].split("-");
+            var ssdomains = sdomains[2].split("-")
+            //if(sdomains.length==4 && dpff1<0) var ssdomains = sdomains[3].split("-"); else var ssdomains = sdomains[2].split("-")
     
             var dpff2 = ssdomains.indexOf("oss");
             //console.log("dpff2: "+dpff2);
     
-            if(dpff1 == 2 && dpff2 == 0 && !sdomains[3]) { //match oss.aliyuncs.com
+            if(dpff1 == 2 && dpff2 == 0 && sdomains[3]) { //match oss.aliyuncs.com
+                var ssdomains = sdomains[3].split("-");
+                var ssdoaminloc = 3;
                 //console.log("oss 3 matched");
-                ssdomains[0] = "oss";
-                ssdomains[1] = "cn";
-                ssdomains[2] = "hangzhou";
-            }
+               // ssdomains[0] = "oss";
+                //ssdomains[1] = "cn";
+                //ssdomains[2] = "hangzhou";
+            } else var ssdoaminloc = 2;
     
-            if(dpff2==0)
-            {
-                //console.log(ssdomains);
+            var dpff3 = ssdomains.indexOf("oss");
+            //console.log("dpff3: "+dpff3);
+    
+            //if(dpff2==0)
+           // {
+                //console.log("regi "+ssdomains);
     
                 //delete bucketname
                 if(sdomains.length==4 && dpff1<0) sdomains.splice(3, 1);
     
-                ssdomains.splice(0, 1); //delete oss from region domain
+                if(dpff3==0) ssdomains.splice(0, 1); //delete oss from region domain
     
-               //console.log(sdomains);
+                if(ossRegions.indexOf(ssdomains.join("-"))!==-1) var isbucket=true; else var isbucket=false;
     
-                /*if(dpff1 == 2 && ssdomains.length == 1) { //match oss.aliyuncs.com
+                //console.log(sdomains);
+    
+                if(dpff1 == 2 && !isbucket) { //match oss.aliyuncs.com
                     ssdomains[0] = "cn";
                     ssdomains[1] = "hangzhou";
                 }
     
-                console.log(ssdomains);*/
+                //console.log(ssdomains);
     
                 //reattach the corrected region
-                if(ssdomains.length!==0) sdomains[2] = ssdomains.join("-"); else sdomains.splice(2, 1);
+                if(ssdomains.length!==0) sdomains[ssdoaminloc] = ssdomains.join("-"); else sdomains.splice(2, 1);
     
                 //console.log(sdomains);
     
-                sdomains.splice(2, 0, "oss");
+                if(dpff3==0) sdomains.splice(2, 0, "oss");
     
                //console.log(sdomains);
-            } else return false;
+            //} else return false;
     
             var fixedhostname = sdomains.reverse().join(".");
     
             return fixedhostname;
     
         } else return false;
-    },
-
+    }
 };
+
+const ossRegions = [
+    "cn-hangzhou",
+    "cn-shanghai",
+    "cn-nanjing",
+    "cn-qingdao",
+    "cn-beijing",
+    "cn-zhangjiakou",
+    "cn-huhehaote",
+    "cn-wulanchabu",
+    "cn-shenzhen",
+    "cn-heyuan",
+    "cn-guangzhou",
+    "cn-chengdu",
+    "cn-hongkong",
+    "us-west-1",
+    "us-east-1",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-southeast-3",
+    "ap-southeast-4",
+    "ap-southeast-5",
+    "ap-southeast-6",
+    "ap-southeast-7",
+    "ap-northeast-1",
+    "ap-northeast-2",
+    "ap-south-1",
+    "eu-central-1",
+    "eu-west-1",
+    "me-east-1",
+    "me-central-1",
+    "cn-hangzhou-finance",
+    "cn-shanghai-finance",
+    "cn-shenzhen-finance",
+    "cn-beijing-finance-1",
+    "cn-beijing-finance-2"
+];
