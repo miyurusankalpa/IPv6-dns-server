f0d8777659493e720c094ed1929d529435a36b29
Miyuru Sankalpa <miyuru@miyuru.lk>
2025-10-30 02:10:41 +0530

    fix fastly fallback

--- app.js
+++ app.js
@@ -416,17 +416,12 @@ function proxy(question, response, cb) {
             function fastly_fallback() {
                 var fsta1 = fastly.check_for_fastly_hostname(last_hostname);
                 //console.log(fsta1);
-                    resolver.resolve6(fsta1, (err, addresses) => {
-                        if (err || !addresses) {
-                            cb();
-                            return;
-                        }
-                        handleResponse(last_type, response, last_hostname, addresses, cb);
-                    });
+                if(fsta1){
                     resolver.resolve6(fsta1, (err, addresses) => {
                         if (addresses != undefined) handleResponse(last_type, response, last_hostname, addresses, cb); else { cb(); return; }
                     });
                     return;
+                }
             }
 
             if (!mse) mse = msedge.check_for_microsoftedge_a(authorityname);
