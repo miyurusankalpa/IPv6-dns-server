213d196c1146d708302e4318da558e4e7916d23d
Miyuru Sankalpa <miyuru@miyuru.lk>
2025-11-20 02:26:32 +0530

    limit generated ipv6 responses to 10
    fixes #42
    
    large packets seem to crash the server. Upstream bug

--- app.js
+++ app.js
@@ -805,9 +805,10 @@ function handleResponse(last_type, response, hostname, ipv6address, cb) {
 
         //for each ipv6 genrate answer
         if (Array.isArray(ipv6address)) {
+            ipv6address = ipv6address.slice(0, 10); //limit to 10 addresses, since it looks like is crashes the server #42
             ipv6address.forEach(ipv6 => {
-                var aaaaresponse = generate_aaaa(hostname, ipv6);
-                response.answer.push(aaaaresponse);
+            var aaaaresponse = generate_aaaa(hostname, ipv6);
+            response.answer.push(aaaaresponse);
             });
         } else {
             var aaaaresponse = generate_aaaa(hostname, ipv6address);
