5c040dcd6c70cfebfda35f86bacb2afd0b079778
Miyuru Sankalpa <miyuru@miyuru.lk>
2026-08-16 11:59:03 +0530

    add new fastly anycast IP ranges 167.82.0.0/17 and 140.248.128.0/17

--- tests/fastly.js
+++ tests/fastly.js
@@ -13,6 +13,16 @@ assert.equal(fastly.getV6HexFromIPv4(["146.75.31.52"],"fastly"), "820"); //dms-f
 assert.equal(fastly.getV6HexFromIPv4(["146.75.47.42"],"fastly"), "810"); //atc.spotify.map.fastly.net.
 assert.equal(fastly.getV6HexFromIPv4(["146.75.107.42"],"fastly"), "810"); //atc.spotify.map.fastly.net.
 assert.equal(fastly.getV6HexFromIPv4(["146.75.93.188"],"fastly"), "444"); //vtg.cbsi.map.fastly.net
+assert.equal(fastly.getV6HexFromIPv4(["167.82.61.140"],"fastly"), "396"); //dualstack.reddit.map.fastly.net
+assert.equal(fastly.getV6HexFromIPv4(["167.82.57.140"],"fastly"), "396"); //dualstack.reddit.map.fastly.net
+
+assert.equal(fastly.check_for_fastly_ip("167.82.61.140"), true);
+assert.equal(fastly.check_for_fastly_ip("167.82.57.140"), true);
+assert.equal(fastly.check_for_fastly_ip("167.82.114.51"), true);
+assert.equal(fastly.check_for_fastly_ip("167.82.128.10"), false);
+assert.equal(fastly.check_for_fastly_ip("140.248.129.10"), true);
+assert.equal(fastly.check_for_fastly_ip("140.248.142.20"), true);
+assert.equal(fastly.check_for_fastly_ip("140.248.64.5"), false);
 
 //github tests
 assert.equal(fastly.getV6HexFromIPv4(["185.199.111.133"],"github"), "133");
